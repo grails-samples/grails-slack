bc09445c2b21eed0e83c28ec1c2e5ff692723ed4
Iván López <lopez.ivan@gmail.com>
2018-01-11 16:53:27 +0100

    Remove duplicated file

--- app/src/main/groovy/org/grails/im/GrailsImEvents.groovy
+++
@@ -1,8 +0,0 @@
-package org.grails.im
-
-class GrailsImEvents {
-
-    static final String NEW_USER = 'newUser'
-    static final String APPROVED_USER = 'approvedUser'
-    static final String REJECTED_USER = 'rejectedUser'
-}
