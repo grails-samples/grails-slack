a22037861e0d14bcd60b8a52014dac17b7de1149
Iván López <lopez.ivan@gmail.com>
2018-01-11 09:11:08 +0100

    Send slack invitation to user

--- grails-app/services/org/grails/im/plugins/slack/SlackNewUserSubscriberService.groovy
+++ grails-app/services/org/grails/im/plugins/slack/SlackNewUserSubscriberService.groovy
@@ -3,21 +3,39 @@ package org.grails.im.plugins.slack
 import grails.config.Config
 import grails.core.support.GrailsConfigurationAware
 import grails.events.annotation.Subscriber
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class SlackNewUserSubscriberService implements GrailsConfigurationAware {
 
+    String apiUrl
     String token
     String channel
 
+    @CompileDynamic
     @Subscriber('newUser')
     void onNewUser(String email, String about) {
-        // TODO notify Skype
+        String url = "${apiUrl}/users.admin.invite?token={token}&channel={channel}&email={email}"
+        Map params = [token: token,
+                      channel: channel,
+                      email: email
+        ]
+
+        RestResponse response = new RestBuilder().get(url) {
+            urlVariables params
+        }
+
+        if (!response.json.ok) {
+            log.error "There was an error inviting the user: ${response.json.error}"
+        }
     }
 
     @Override
     void setConfiguration(Config co) {
+        apiUrl = co.getProperty('slack.apiUrl', String)
         token = co.getProperty('slack.token', String)
         channel = co.getProperty('slack.channel', String)
     }
