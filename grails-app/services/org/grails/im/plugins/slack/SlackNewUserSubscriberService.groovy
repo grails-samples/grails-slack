eb3fbeb01b5aefc3fb0684ea51e4a83cf7596f85
Iván López <lopez.ivan@gmail.com>
2018-01-11 15:11:33 +0100

    Service to post the request to approve/reject user into slack channel

--- grails-app/services/org/grails/im/plugins/slack/SlackNewUserSubscriberService.groovy
+++ grails-app/services/org/grails/im/plugins/slack/SlackNewUserSubscriberService.groovy
@@ -1,24 +1,80 @@
 package org.grails.im.plugins.slack
 
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 import grails.events.annotation.Subscriber
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.grails.im.GrailsImEvents
 import org.grails.im.entities.RequestInvite
 
 @CompileStatic
-class SlackNewUserSubscriberService {
+class SlackNewUserSubscriberService implements GrailsConfigurationAware {
 
-    SlackApprovalRequestComposerService slackApprovalRequestComposerService
+    String apiUrl
+    String token
+    String channel
+    String callbackUrl
+
+    @Override
+    void setConfiguration(Config co) {
+        apiUrl = co.getProperty('slack.apiUrl', String)
+        token = co.getProperty('slack.token', String)
+        channel = co.getProperty('slack.channel', String)
+        callbackUrl = co.getProperty('slack.callbackUrl', String)
+    }
 
     @Subscriber(GrailsImEvents.NEW_USER)
     void onNewUser(RequestInvite requestInvite) {
-        if (requestInvite != null) {
-            SlackApproveMessage slackApproveMessage = slackApprovalRequestComposerService.compose(requestInvite)
-            send(slackApproveMessage)
-        }
+        send(requestInvite)
     }
 
-    void send(SlackApproveMessage slackApproveMessage) {
+    @CompileDynamic
+    void send(RequestInvite requestInvite) {
+        String url = "${apiUrl}/chat.postMessage?token={token}&channel={channel}&text={text}&attachments={attachments}"
+        String attachments = """
+[{
+    "fallback": "You can not approve users",
+    "callback_id": "approver",
+    "color": "#3AA3E3",
+    "attachment_type": "default",
+    "response_url": "${callbackUrl}",
+    "actions": [
+        {
+            "name": "Approve",
+            "text": "Approve",
+            "style": "primary",
+            "type": "button",
+            "value": "${requestInvite.email}"
+        },
+        {
+            "name": "Reject",
+            "text": "Reject",
+            "style": "danger",
+            "type": "button",
+            "value": "${requestInvite.email}"
+        }
+    ]
+}]
+"""
 
+        String text = "*user:* ${requestInvite.email}\n*about:* ${requestInvite.about}"
+
+        Map<String, String> params = [
+            token      : token,
+            channel    : channel,
+            text       : text,
+            attachments: attachments,
+        ]
+
+        RestResponse response = new RestBuilder().get(url) {
+            urlVariables params
+        }
+
+        if (!response.json.ok) {
+            log.error "There was an error inviting the user: ${response.json.error}"
+        }
     }
 }
--- src/main/groovy/org/grails/im/GrailsImEvents.groovy
+++ src/main/groovy/org/grails/im/GrailsImEvents.groovy
@@ -2,7 +2,7 @@ package org.grails.im
 
 class GrailsImEvents {
 
-    static final String NEW_USER = 'newUser'
-    static final String APPROVED_USER = 'approvedUser'
-    static final String REJECTED_USER = 'rejectedUser'
+    public static final String NEW_USER = 'newUser'
+    public static final String APPROVED_USER = 'approvedUser'
+    public static final String REJECTED_USER = 'rejectedUser'
 }
