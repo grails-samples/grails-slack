bc60b941cf1472efd7e8c7821a75c16f5f351c74
sdelamo <sergio.delamo@softamo.com>
2018-01-12 11:48:16 +0100

    Publish docs to gh-pages
    Fixes https://github.com/grails-samples/grails-slack/issues/5

--- travis-build.sh
+++ travis-build.sh
@@ -17,6 +17,27 @@ if [[ $EXIT_STATUS -ne 0 ]]; then
     exit $EXIT_STATUS
 fi
 
+# If this is the master branch then update the snapshot
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+    git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+    cd gh-pages
+
+    cp -r ../docs/build/asciidoc/html5/. ./snapshot/
+    git add snapshot/*
+
+    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+
+    cd ..
+    rm -rf gh-pages
+fi
+
 echo "Tag: $TRAVIS_TAG"
 
 if [[ $EXIT_STATUS ]]; then
