2d264584d9ad084b0f6c725af42ff47672418e90
Iván López <lopez.ivan@gmail.com>
2018-01-15 15:21:51 +0100

    Add CO to validate Slack callback parameters

---
+++ app/grails-app/controllers/org/grails/im/plugins/api/controllers/slack/SlackCallbackCommand.groovy
@@ -0,0 +1,38 @@
+package org.grails.im.plugins.api.controllers.slack
+
+import grails.validation.Validateable
+import groovy.json.JsonSlurper
+
+class SlackCallbackCommand implements Validateable {
+
+    String payload
+    private Map<String, Object> parsedJson
+
+    static constraints = {
+        payload nullable: false, blank: false
+    }
+
+    String fetchToken() {
+        parseJson()?.token
+    }
+
+    String fetchAction() {
+        parseJson()?.actions?.first()?.name
+    }
+
+    String fetchEmail() {
+        parseJson()?.actions?.first()?.value
+    }
+
+    String fetchUser() {
+        parseJson()?.user?.name
+    }
+
+    private Map<String, Object> parseJson() {
+        if (!parsedJson) {
+            parsedJson = new JsonSlurper().parse(payload.bytes) as Map
+        }
+
+        return parsedJson
+    }
+}
