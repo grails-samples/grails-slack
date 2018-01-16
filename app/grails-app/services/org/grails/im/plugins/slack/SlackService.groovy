2998f289003dad3a64ab85b0ba4ef9a741cbe4f8
sdelamo <sergio.delamo@softamo.com>
2018-01-16 07:49:44 +0100

    Make method public

--- app/grails-app/services/org/grails/im/plugins/slack/SlackService.groovy
+++ app/grails-app/services/org/grails/im/plugins/slack/SlackService.groovy
@@ -68,7 +68,7 @@ class SlackService implements GrailsConfigurationAware {
     }
 
     @CompileDynamic
-    private String generateAttachments(String email) {
+    String generateAttachments(String email) {
         new StringWriter().with { w ->
             def builder = new StreamingJsonBuilder(w)
 
